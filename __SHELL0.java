fde10fd2317dcd89be6b8207e7d94b7aad9aec44
Anal Shrestha <annalshrestha999@gmail.com>
2025-12-06 21:20:27 +0545

    hi

--- __SHELL0.java
+++ __SHELL0.java
@@ -3,6 +3,6 @@ public class __SHELL0 extends bluej.runtime.Shell {
 public static void run() throws Throwable {
 
 java.lang.String[] __bluej_param0 = { };
-scenario.main(__bluej_param0);
+compound.main(__bluej_param0);
 
 }}
